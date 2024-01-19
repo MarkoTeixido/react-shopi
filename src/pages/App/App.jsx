deb7479550eecc91c9a1d27b7344337cb45e1051
MarkoTeixido <teixido_marko@outlook.es>
2024-01-19 04:31:19 -0300

    [INIT] - sorting directory

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,10 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom';
-import Home from '../Home';
-import MyAccount from '../MyAccount';
-import MyOrder from '../MyOrder';
-import MyOrders from '../MyOrders';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
+import Home from '../Home/Home';
+import MyAccount from '../MyAccount/MyAccount';
+import MyOrder from '../MyOrder/MyOrder';
+import MyOrders from '../MyOrders/MyOrders';
+import NotFound from '../NotFound/NotFound';
+import SignIn from '../SignIn/SignIn';
 import '../../index.css';
 
 const AppRoutes = () => {
