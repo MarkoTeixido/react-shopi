619d414e44d5c1fe628478ba847298e4636ecde3
MarkoTeixido <teixido_marko@outlook.es>
2024-01-23 23:21:33 -0300

    [FEAT] - Cart, MyOrder and CountProducts done

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,7 +3,6 @@ import CheckoutMenu from "../../components/CheckoutMenu/CheckoutMenu";
 function Cart() {
     return (
       <div className="flex flex-col">
-        MyOrder
         <CheckoutMenu />
       </div>
     );
